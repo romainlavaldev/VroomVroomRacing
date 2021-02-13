c9b2fd8466b3800406b5c46e94db6e657ff4d5e5
romain <romain.laval000@gmail.com>
2021-02-13 18:17:21 +0100

    Boutton Quitter

--- src/mainMenuTest.c
+++ src/mainMenuTest.c
@@ -29,7 +29,7 @@ int main(int argc, char** argv)
    }
 
     Mix_Music* mainMenuMusic = NULL;
-    mainMenuMusic = Mix_LoadMUS("mainMenu.mp3");
+    mainMenuMusic = Mix_LoadMUS("sound/music/mainMenu.mp3");
 
         printf("%s\n", Mix_GetError()); //coulnd't open ".mp3"
 
@@ -52,27 +52,89 @@ int main(int argc, char** argv)
         //
         
         SDL_Renderer* renderer = SDL_CreateRenderer(pWindow, -1, 0);
-        SDL_RenderSetLogicalSize(renderer, 1280, 720);
+        SDL_RenderSetLogicalSize(renderer, 1920, 1080);
 
 
-        SDL_Surface* bgtmp = SDL_LoadBMP("img/fond.bmp");
-        SDL_Texture* bgTexture = SDL_CreateTextureFromSurface(renderer, bgtmp);
-        SDL_Rect positionBg;
-        positionBg.x = 0;
-        positionBg.y = 0;
-        positionBg.w = 1280;
-        positionBg.h = 720;
-        SDL_RenderCopy(renderer, bgTexture, NULL, &positionBg);
+
+        //CREATION DES IMAGES ET BOUTTONS
+
+        //Fond
+        SDL_Surface* bgTmp = SDL_LoadBMP("img/fond.bmp");
+        SDL_Texture* bgTexture = SDL_CreateTextureFromSurface(renderer, bgTmp);
+        SDL_FreeSurface(bgTmp);
+        SDL_Rect bgPosition;
+        bgPosition.x = 0;
+        bgPosition.y = 0;
+        bgPosition.w = 1920;
+        bgPosition.h = 1080;
+        SDL_RenderCopy(renderer, bgTexture, NULL, &bgPosition);
+
+        //Boutton Solo
+        SDL_Surface* soloButtonTmp = SDL_LoadBMP("img/solo.bmp");
+        SDL_Texture* soloButtonTexture = SDL_CreateTextureFromSurface(renderer, soloButtonTmp);
+        SDL_FreeSurface(soloButtonTmp);
+        SDL_Rect soloButtonPosition;
+        soloButtonPosition.x = 586;
+        soloButtonPosition.y = 319;
+        soloButtonPosition.w = 1334;
+        soloButtonPosition.h = 150;
+        SDL_RenderCopy(renderer, soloButtonTexture, NULL, &soloButtonPosition);
+
+        //Boutton Multijoueur
+        SDL_Surface* multiButtonTmp = SDL_LoadBMP("img/multi.bmp");
+        SDL_Texture* multiButtonTexture = SDL_CreateTextureFromSurface(renderer, multiButtonTmp);
+        SDL_FreeSurface(multiButtonTmp);
+        SDL_Rect multiButtonPosition;
+        multiButtonPosition.x = 586;
+        multiButtonPosition.y = 504;
+        multiButtonPosition.w = 1334;
+        multiButtonPosition.h = 150;
+        SDL_RenderCopy(renderer, multiButtonTexture, NULL, &multiButtonPosition);
+
+        //Boutton Quitter
+        SDL_Surface* quitButtonTmp = SDL_LoadBMP("img/quitter.bmp");
+        SDL_Texture* quitButtonTexture = SDL_CreateTextureFromSurface(renderer, quitButtonTmp);
+        SDL_FreeSurface(quitButtonTmp);
+        SDL_Rect quitButtonPosition;
+        quitButtonPosition.x = 1030;
+        quitButtonPosition.y = 901;
+        quitButtonPosition.w = 890;
+        quitButtonPosition.h = 100;
+
+
+        //Boutton QuitterHover
+        SDL_Surface* quitHoverButtonTmp = SDL_LoadBMP("img/quitterHover.bmp");
+        SDL_Texture* quitHoverButtonTexture = SDL_CreateTextureFromSurface(renderer, quitHoverButtonTmp);
+        SDL_FreeSurface(quitHoverButtonTmp);
+        SDL_Rect quitHoverButtonPosition;
+        quitHoverButtonPosition.x = 1030;
+        quitHoverButtonPosition.y = 901;
+        quitHoverButtonPosition.w = 890;
+        quitHoverButtonPosition.h = 100;
+
                                                                   
 /*******************************************************************************
  *  MENU 
  ******************************************************************************/
         if( pWindow )
         {
+
             SDL_Event event;
+
             Mix_PlayMusic(mainMenuMusic, -1);
+
             int run=1;
             while (run){
+
+                int cursorX = event.button.x;
+                int cursosY = event.button.y;
+
+                if( (cursorX > quitButtonPosition.x) && (cursorX < quitButtonPosition.x + quitButtonPosition.w) && (cursosY > quitButtonPosition.y) && (cursosY < quitButtonPosition.y + quitButtonPosition.h) ){
+                    SDL_RenderCopy(renderer, quitHoverButtonTexture, NULL, &quitHoverButtonPosition);
+                }else{
+                    SDL_RenderCopy(renderer, quitButtonTexture, NULL, &quitButtonPosition);
+                }
+
                 SDL_RenderPresent(renderer);
                 while (SDL_PollEvent(&event)){
                     switch (event.type){
@@ -82,6 +144,22 @@ int main(int argc, char** argv)
                         Mix_CloseAudio();
                         SDL_Quit(); // fermeture SDL
                         run=0;
+                        break;
+
+                    case SDL_MOUSEBUTTONUP:
+                        if (event.button.button == SDL_BUTTON_LEFT){
+                            printf("\nCursorX : %d   CursorY : %d\n", cursorX, cursosY);
+
+                            //Zone du boutton quitter
+                            if( (cursorX > quitButtonPosition.x) && (cursorX < quitButtonPosition.x + quitButtonPosition.w) && (cursosY > quitButtonPosition.y) && (cursosY < quitButtonPosition.y + quitButtonPosition.h) ){
+                                printf("c'est tchao");
+                                SDL_DestroyWindow(pWindow); // fermeture de fenêtre
+                                Mix_FreeMusic(mainMenuMusic);
+                                Mix_CloseAudio();
+                                SDL_Quit(); // fermeture SDL
+                                run=0;
+                            }
+                        }
                         break;
                     default:
                         break;
